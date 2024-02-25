ac5824bcef4500bad3eb604286dfec0f06f2a233
zalesiu <zalewmat31@gmail.com>
2024-02-25 19:43:03 +0100

    feat: add userRouter to index

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import { userRouter } from "./routes/user";
 const connectDB = require("./db/connect");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/user", userRouter);
 
 const startServer = async () => {
   try {
