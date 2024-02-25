f8eb8056d7f2e5f466b7cd35288a7a7c4cbdde55
zalesiu <zalewmat31@gmail.com>
2024-02-25 19:33:26 +0100

    feat: add middleware to index

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,12 @@
 import express from "express";
+import cors from "cors";
 import "dotenv/config";
 const connectDB = require("./db/connect");
 
 const app = express();
+
+app.use(express.json());
+app.use(cors());
 
 const startServer = async () => {
   try {
