85a1e7787f108a71a784e44612efafeb16be7f0f
zalesiu <zalewmat31@gmail.com>
2024-02-27 17:53:00 +0100

    feat: add login user route

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,7 +1,9 @@
 import { Router, Request, Response } from "express";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import { UserModel } from "../models/user";
 import { UserErrors } from "../errors";
+import { IUser } from "../types";
 
 const router = Router();
 
@@ -9,7 +11,7 @@ router.post("/register", async (req: Request, res: Response) => {
   const { username, password } = req.body;
 
   try {
-    const user = await UserModel.findOne({ username });
+    const user: IUser = await UserModel.findOne({ username });
 
     if (user) {
       return res.status(400).json({ type: UserErrors.USERNAME_ALREADY_EXISTS });
@@ -19,9 +21,31 @@ router.post("/register", async (req: Request, res: Response) => {
     const newUser = new UserModel({ username, password: hashedPassword });
     await newUser.save();
 
-    res.json({ message: "User Registered Successfully" });
+    res.status(200).json({ message: "User Registered Successfully" });
   } catch (err) {
     res.status(500).json({ type: err });
+  }
+});
+
+router.post("/login", async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+
+  try {
+    const user: IUser = await UserModel.findOne({ username });
+
+    if (!user) {
+      return res.status(400).json({ type: UserErrors.NO_USER_FOUND });
+    }
+
+    const isValidPassword = await bcrypt.compare(password, user.password);
+
+    if (!isValidPassword) {
+      return res.status(400).json({ type: UserErrors.WRONG_CREDENTIALS });
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    res.status(200).json({ token, userID: user._id });
+  } catch (error) {
+    res.status(500).json({ error });
   }
 });
 
