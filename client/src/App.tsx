88ce2b41cbb8239fad8e9e942d257c1859e08992
zalesiu <zalewmat31@gmail.com>
2024-02-28 21:06:33 +0100

    feat: add navbar and routes to App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,25 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
+import AuthPage from "./pages/auth";
+import CheckoutPage from "./pages/checkout";
+import PurchasedItemsPage from "./pages/purchased-items";
+import ShopPage from "./pages/shop";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<ShopPage />} />
+          <Route path="/auth" element={<AuthPage />} />
+          <Route path="/checkout" element={<CheckoutPage />} />
+          <Route path="/purchased-items" element={<PurchasedItemsPage />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
