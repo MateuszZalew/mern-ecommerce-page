9c54f150ae92a933bcc49cc94803cf6d06e98a41
zalesiu <zalewmat31@gmail.com>
2024-03-03 19:36:10 +0100

    feat: add checkout endpoint to product routes

--- server/src/routes/product.ts
+++ server/src/routes/product.ts
@@ -14,4 +14,52 @@ router.get("/", async (_, res: Response) => {
   }
 });
 
+router.post("/checkout", async (req: Request, res: Response) => {
+  const { customerID, cartItems } = req.body;
+
+  try {
+    const user = await UserModel.findById(customerID);
+    const productIDs = Object.keys(cartItems);
+    const products = await ProductModel.find({ _id: { $in: productIDs } });
+
+    if (!user) {
+      return res.status(400).json({ type: UserErrors.NO_USER_FOUND });
+    }
+
+    if (products.length !== productIDs.length) {
+      return res.status(400).json({ type: ProductErrors.NO_PRODUCT_FOUND });
+    }
+
+    let totalPrice = 0;
+    for (const item in cartItems) {
+      const product = products.find((product) => String(product._id) === item);
+
+      if (!product) {
+        return res.status(400).json({ type: ProductErrors.NO_PRODUCT_FOUND });
+      }
+
+      if (product.stockQuantity < cartItems[item]) {
+        return res.status(400).json({ type: ProductErrors.OUT_OF_STOCK });
+      }
+
+      totalPrice += product.price * cartItems[item];
+    }
+
+    if (user.availableMoney < totalPrice) {
+      return res.status(400).json({ type: ProductErrors.NO_AVAILABLE_MONEY });
+    }
+
+    user.availableMoney -= totalPrice;
+    user.purchasedItems.push(...productIDs);
+    await user.save();
+    await ProductModel.updateMany(
+      { _id: { $in: productIDs } },
+      { $inc: { stockQuantity: -1 } }
+    );
+    res.json({ purchasedItems: user.purchasedItems });
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+});
+
 export { router as productRouter };
