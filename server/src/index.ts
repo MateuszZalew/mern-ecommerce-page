cd94e48bb12b08ce15d30304d9263319b87fc1ed
zalesiu <zalewmat31@gmail.com>
2024-03-03 19:14:29 +0100

    feat: add productRouter to index

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import { userRouter } from "./routes/user";
+import { productRouter } from "./routes/product";
 const connectDB = require("./db/connect");
 
 const app = express();
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/user", userRouter);
+app.use("/product", productRouter);
 
 const startServer = async () => {
   try {
