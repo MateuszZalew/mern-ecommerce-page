638616b5beeb2b2b2853a2fe75e20632bee3645f
zalesiu <zalewmat31@gmail.com>
2024-03-03 18:38:37 +0100

    feat: add purchasedItems to IUser interface, create IProduct

--- server/src/types.ts
+++ server/src/types.ts
@@ -3,4 +3,13 @@ export interface IUser {
   username: string;
   password: string;
   availableMoney: number;
+  purchasedItems: string[];
+}
+
+export interface IProduct {
+  productName: string;
+  price: number;
+  description: string;
+  imageURL: string;
+  stockQuantity: number;
 }
