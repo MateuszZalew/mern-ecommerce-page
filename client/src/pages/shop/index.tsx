6cfe6e838e1f8539a309131b26af2faa924a6116
zalesiu <zalewmat31@gmail.com>
2024-03-07 22:17:04 +0100

    feat: add content to shop components, add styling

--- client/src/pages/shop/index.tsx
+++ client/src/pages/shop/index.tsx
@@ -1,5 +1,18 @@
+import useGetProducts from "../../hooks/useGetProducts";
+import Product from "./product";
+import "./styles.css";
+
 const ShopPage = () => {
-  return <div>Shop</div>;
+  const { products } = useGetProducts();
+  return (
+    <div className="shop">
+      <div className="products">
+        {products.map((product) => (
+          <Product product={product} key={product._id} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default ShopPage;
---
+++ client/src/pages/shop/product.tsx
@@ -0,0 +1,35 @@
+import { useContext } from "react";
+import { IProduct } from "../../models/interfaces";
+import "./styles.css";
+import { IShopContext, ShopContext } from "../../context/shop-context";
+
+interface Props {
+  product: IProduct;
+}
+
+const Product = (props: Props) => {
+  const { _id, productName, description, price, stockQuantity, imageURL } =
+    props.product;
+
+  const { addToCart, getCartItemCount } = useContext<IShopContext>(ShopContext);
+
+  const count = getCartItemCount(_id);
+  return (
+    <div className="product">
+      <img src={imageURL} alt={productName} />
+      <div className="description">
+        <h3>{productName}</h3>
+        <p>{description}</p>
+        <p>${price}</p>
+      </div>
+      <button className="add-to-cart-btn" onClick={() => addToCart(_id)}>
+        Add to Cart {count > 0 && <>({count})</>}
+      </button>
+      <div className="stock-quantity">
+        {stockQuantity === 0 && <h1>OUT OF STOCK</h1>}
+      </div>
+    </div>
+  );
+};
+
+export default Product;
