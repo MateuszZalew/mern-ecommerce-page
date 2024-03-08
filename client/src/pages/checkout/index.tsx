8f7a86032904336500792d7fa8305b4a221238ac
zalesiu <zalewmat31@gmail.com>
2024-03-08 17:18:48 +0100

    feat: add conditional rendering if totalAmount is more than zero in checkout page

--- client/src/pages/checkout/index.tsx
+++ client/src/pages/checkout/index.tsx
@@ -3,10 +3,15 @@ import useGetProducts from "../../hooks/useGetProducts";
 import { IShopContext, ShopContext } from "../../context/shop-context";
 import CartItem from "./cart-item";
 import "./styles.css";
+import { useNavigate } from "react-router-dom";
 
 const CheckoutPage = () => {
-  const { getCartItemCount } = useContext<IShopContext>(ShopContext);
+  const { getCartItemCount, getTotalCartAmount, checkout } =
+    useContext<IShopContext>(ShopContext);
   const { products } = useGetProducts();
+  const navigate = useNavigate();
+
+  const totalAmount = getTotalCartAmount();
 
   return (
     <div className="cart">
@@ -21,6 +26,16 @@ const CheckoutPage = () => {
           }
         })}
       </div>
+
+      {totalAmount > 0 ? (
+        <div className="checkout">
+          <p>Subtotal: ${totalAmount.toFixed(2)}</p>
+          <button onClick={() => navigate("/")}>Continue Shopping</button>
+          <button onClick={checkout}>Checkout</button>
+        </div>
+      ) : (
+        <h1>Your shopping Cart is Empty</h1>
+      )}
     </div>
   );
 };
