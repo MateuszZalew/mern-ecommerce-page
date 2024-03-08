0c0163f831b113b28b014dc0160cd4f99fa8d625
zalesiu <zalewmat31@gmail.com>
2024-03-08 15:37:16 +0100

    feat: update checkout components, add functionality to modify number of items in cart

--- client/src/pages/checkout/index.tsx
+++ client/src/pages/checkout/index.tsx
@@ -1,5 +1,28 @@
+import { useContext } from "react";
+import useGetProducts from "../../hooks/useGetProducts";
+import { IShopContext, ShopContext } from "../../context/shop-context";
+import CartItem from "./cart-item";
+import "./styles.css";
+
 const CheckoutPage = () => {
-  return <div>Checkout</div>;
+  const { getCartItemCount } = useContext<IShopContext>(ShopContext);
+  const { products } = useGetProducts();
+
+  return (
+    <div className="cart">
+      <div>
+        <h1>Your Cart Items</h1>
+      </div>
+
+      <div className="cart">
+        {products.map((product) => {
+          if (getCartItemCount(product._id) !== 0) {
+            return <CartItem product={product} key={product._id} />;
+          }
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default CheckoutPage;
