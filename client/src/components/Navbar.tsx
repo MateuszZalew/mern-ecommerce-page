f57a8f1237f142edf4311acd7938e87182498c8b
zalesiu <zalewmat31@gmail.com>
2024-03-08 15:28:36 +0100

    feat: add checkout icon and update content in Navbar component

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,16 +1,26 @@
 import { Link } from "react-router-dom";
+import { BsCart3 } from "react-icons/bs";
+import "./styles.css";
 
 export const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navbar-title">
-        <h1>Mateusz Zalewski</h1>
+        <h1>E-Commerce Shop</h1>
       </div>
 
       <div className="navbar-links">
-        <Link to="/">Shop</Link>
-        <Link to="/purchased-items">Purchases</Link>
-        <Link to="/checkout">Checkout</Link>
+        <Link to="/">
+          <div className="link-container">Shop</div>
+        </Link>
+        <Link to="/purchased-items">
+          <div className="link-container">Purchases</div>
+        </Link>
+        <Link to="/checkout">
+          <div className="link-container">
+            <BsCart3 />
+          </div>
+        </Link>
       </div>
     </div>
   );
