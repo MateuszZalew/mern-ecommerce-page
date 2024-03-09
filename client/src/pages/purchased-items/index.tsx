513334f90a8ecb1cdaaad22ede5c8ec47fc1c1bb
zalesiu <zalewmat31@gmail.com>
2024-03-09 14:02:13 +0100

    feat: add purchased items view with styles

--- client/src/pages/purchased-items/index.tsx
+++ client/src/pages/purchased-items/index.tsx
@@ -1,5 +1,32 @@
+import { useContext } from "react";
+import { IShopContext, ShopContext } from "../../context/shop-context";
+import "./styles.css";
+
 const PurchasedItemsPage = () => {
-  return <div>PurchasedItems</div>;
+  const { purchasedItems, getCartItemCount, addToCart } =
+    useContext<IShopContext>(ShopContext);
+
+  return (
+    <div className="purchased-items-page">
+      <h1>Previously Purchased Items</h1>
+      <div className="purchased-items">
+        {purchasedItems.map((item) => {
+          const { _id, productName, imageURL, price } = item;
+          const count = getCartItemCount(_id);
+          return (
+            <div className="item">
+              <h3>{productName}</h3>
+              <img src={imageURL} alt={productName} />
+              <p>$ {price}</p>
+              <button onClick={() => addToCart(_id)}>
+                Purchase Again {count > 0 && <>({count})</>}
+              </button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default PurchasedItemsPage;
