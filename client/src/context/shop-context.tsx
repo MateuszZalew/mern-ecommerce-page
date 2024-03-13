fcbafb9b3f70180eaa53943987f0713a6dea2cba
zalesiu <zalewmat31@gmail.com>
2024-03-13 23:53:41 +0100

    feat: add error alert on cart checkout

--- client/src/context/shop-context.tsx
+++ client/src/context/shop-context.tsx
@@ -135,7 +135,8 @@ export const ShopContextProvider = (props) => {
       fetchPurchasedItems();
       navigate("/");
     } catch (err) {
-      console.log(err);
+      console.log(err.response.data.type);
+      alert(err.response.data.type);
     }
   };
 
