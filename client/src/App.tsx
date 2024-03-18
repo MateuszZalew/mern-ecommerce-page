654f531a071ad958ef140cc1ff672a7eb44263dd
zalesiu <zalewmat31@gmail.com>
2024-03-18 04:26:37 +0100

    feat: add ToastContainer component, make separate routes for login and register pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Navbar } from "./components/Navbar";
-import AuthPage from "./pages/auth";
+import Navbar from "./components/navbar";
+import LoginPage from "./pages/auth/login";
+import RegisterPage from "./pages/auth/register";
 import CheckoutPage from "./pages/checkout";
 import PurchasedItemsPage from "./pages/purchased-items";
 import ShopPage from "./pages/shop";
 import { ShopContextProvider } from "./context/shop-context";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
@@ -15,11 +17,18 @@ function App() {
           <Navbar />
           <Routes>
             <Route path="/" element={<ShopPage />} />
-            <Route path="/auth" element={<AuthPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
             <Route path="/checkout" element={<CheckoutPage />} />
             <Route path="/purchased-items" element={<PurchasedItemsPage />} />
           </Routes>
         </ShopContextProvider>
+        <ToastContainer
+          position="top-center"
+          autoClose={2500}
+          theme="light"
+          closeOnClick
+        />
       </Router>
     </div>
   );
